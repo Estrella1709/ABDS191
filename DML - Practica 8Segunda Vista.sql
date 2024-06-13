23501a69c53e02e95b79e47f4b12f825f6d55829
Estrella1709 <estrellatorrijos17@gmail.com>
2024-06-12 18:50:15 -0600

    DML - Correccion Vista 2 Prac. 8

--- DML - Practica 8Segunda Vista.sql
+++ DML - Practica 8Segunda Vista.sql
@@ -1,4 +1,4 @@
-CREATE VIEW Vista_ComprasComic AS
+CREATE VIEW Vista_ComiscComprados AS
 SELECT c.id_cliente, c.nombre AS 'Nombre cliente', cc.cantidad AS 'Cantidad de comics comprados'
 FROM Clientes c
 JOIN Comic_Compras cc
